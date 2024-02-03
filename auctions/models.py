67e84ea4d837a022d4223b75e2bf2c3e816452fc
syeo-1 <seansqyeo@gmail.com>
2024-02-03 15:36:07 -0500

    initial listing model and primary key settings

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    # title
+    # description
+    # starting bid
+    # image_url
+    # category
+    title = models.CharField(max_length = 64)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.CharField(max_length=512)
+    category = models.CharField(max_length=64)
+
+# class Bid(models.Model):
+#     bid_price = models.DecimalField(max_digits=10, decimal_places=2)
+#     # the bid should be related to a specific listing
+#     # specifically, should have details for the listing you're bidding on 
+
+# class Comment(models.Model):
+#     comment = models.TextField()
