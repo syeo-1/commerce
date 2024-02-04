054d66535872aa28d51a17b6a78f96e3f2f9bb2a
syeo-1 <seansqyeo@gmail.com>
2024-02-04 16:41:39 -0500

    have basic active listings page up with css

--- auctions/models.py
+++ auctions/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime
 
 
 class User(AbstractUser):
@@ -11,11 +12,14 @@ class Listing(models.Model):
     # starting bid
     # image_url
     # category
+    current_time = datetime.now()
     title = models.CharField(max_length = 64)
     description = models.TextField()
-    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.CharField(max_length=512)
     category = models.CharField(max_length=64)
+    creation_time = models.DateTimeField(auto_now_add=True)
+
 
 # class Bid(models.Model):
 #     bid_price = models.DecimalField(max_digits=10, decimal_places=2)
