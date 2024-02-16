639bf9be929cc0a411b6d0a230f61eedb9ca2145
syeo-1 <seansqyeo@gmail.com>
2024-02-15 23:44:34 -0500

    add basic watchlist functionality

--- auctions/urls.py
+++ auctions/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+    path('listings/<int:listing_id>/add_to_watchlist', views.add_to_watchlist, name='add_to_watchlist'),
+    path('listings/<int:listing_id>/remove_from_watchlist', views.remove_from_watchlist, name='remove_from_watchlist'),
     path("listings/<int:listing_id>", views.listing, name='listing'),
-    path('create_listing', views.create_listing, name='create_listing')
+    path('create_listing', views.create_listing, name='create_listing'),
+    path('watchlist', views.watchlist, name='watchlist')
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -7,18 +7,37 @@ from django.urls import reverse
 from .models import User
 from .models import Listing
 
-
 def index(request):
-    print(request.user)
-    print(request.user.id)
+    # print(request.user)
+    # print(request.user.id)
     return render(request, "auctions/index.html", {
         'listings': Listing.objects.all()
     })
 
 def listing(request, listing_id):
     listing = Listing.objects.get(id=listing_id)
+    # should also be doing a check to see if the listing is part of the user's watchlist
+    # if the listing isn't part of their watchlist, should have a bool which is passed to the template
+    # in the template, can then render the remove or add to watchlist button based on that info
+
+    # check if the listing is in the current user's watchlist
+    user = User.objects.get(username=request.user)
+    print(request)
+    print(f'username is {user}')
+    in_user_watchlist = False
+    user_watchlist = [listing for listing in user.listings.all()]
+
+    if listing in user_watchlist:
+        in_user_watchlist = True
+    # in_user_watchlist = True
+    print(listing)
+    print(user_watchlist)
+    
+    print(f'in listing: {in_user_watchlist}')
+
     return render(request, 'auctions/listing.html', {
-        'listing': listing
+        'listing': listing,
+        'in_watchlist': in_user_watchlist
     })
 
 def create_listing(request):
@@ -31,10 +50,45 @@ def create_listing(request):
         new_listing.current_price = request.POST.get('current_price')
         new_listing.image_url = request.POST.get('image_url')
         new_listing.category = request.POST.get('category')
+        # new_listing.associated_user = request.user
         
         new_listing.save()
 
         return HttpResponseRedirect(reverse('listing', args=(new_listing.id,)))
+
+def watchlist(request):
+    # get the current logged in user
+    user = User.objects.get(username=request.user)
+    print(request)
+    print(f'username is {user}')
+
+    # get all listing associated with the user
+    watchlist_listings = [listing for listing in user.listings.all()]
+
+    # return all associated listings
+    # return render(request, 'auctions/watchlist.html', {
+    #     'watchlist': user.listings.all()
+    # })
+    return render(request, "auctions/watchlist.html", {
+        'listings': watchlist_listings
+    })
+
+def add_to_watchlist(request, listing_id):
+    # check 1:29:20
+    if request.method == 'POST':
+        print('teststetsets')
+        listing = Listing.objects.get(pk=listing_id)
+        user = User.objects.get(username=request.user)
+        user.listings.add(listing)
+        return HttpResponseRedirect(reverse('listing', args=(listing_id,)))
+
+def remove_from_watchlist(request, listing_id):
+    if request.method == 'POST':
+        listing = Listing.objects.get(pk=listing_id)
+        user = User.objects.get(username=request.user)
+        user.listings.remove(listing)
+        return HttpResponseRedirect(reverse('listing', args=(listing_id,)))
+
 
 def login_view(request):
     if request.method == "POST":
