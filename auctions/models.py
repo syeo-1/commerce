e7a0b64276d73c5dc3b6eea18a9bf85204bcb211
syeo-1 <seansqyeo@gmail.com>
2024-02-19 19:02:31 -0500

    add is_active field to listing model

--- auctions/models.py
+++ auctions/models.py
@@ -20,6 +20,7 @@ class Listing(models.Model):
     category = models.CharField(max_length=64)
     creation_time = models.DateTimeField(auto_now_add=True)
     associated_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listing_creator')
+    is_active = models.BooleanField(default=True)
 
 class Bid(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
